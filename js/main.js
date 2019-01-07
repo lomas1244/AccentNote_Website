ea76b9a5916bf7e64e4668eddbd44db2711ef0e9
lomas1244 <jameslomas2004@gmail.com>
2019-01-07 02:52:26 +0000

    initial website files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+	$(window).scroll(function() {
+		$('.n-1').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
